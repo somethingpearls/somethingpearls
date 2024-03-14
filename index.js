8059966ea2dc2d77f7a9da3b64262245a8c95c05
somethingpearls <yuruchenpearl@gmail.com>
2024-03-14 19:37:13 +0900

    until 48.maps.

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ document.getElementById("currentDateTime1").textContent = currentDate1;
 let x = document.getElementById("votre-nom-famille-input");
 let y = document.getElementById("votre-nom-input");
 
-document.getElementById("votre-nom-btn").onclick = function(){
+document.getElementById("votre-nom-btn myBtn").onclick = function(){
     const yourName = x.value + " " + y.value;
     console.log(yourName);
     document.getElementById("salutation").innerHTML = `bonjour ${yourName} mon ami ;) ` ; 
 }
-const yourName = x.value + " " + y.value;
-const salutation2 = yourName => `mon ami ${yourName} qu'est ce que vous voulez faire aujourd'hui? `
+let yourName = x.value + " " + y.value;
+let salutation2 = yourName => `mon ami ${yourName} qu'est ce que vous voulez faire aujourd'hui? `
 document.getElementById("salutation2Html").innerHTML = salutation2(yourName);
 
 // loop through an array , sort , 2D array, spread operator , 
@@ -34,7 +34,7 @@ function displayNameCuteFriends() {
     document.getElementById("namesCuteFriends").innerHTML = "mes amis sont :" + "<ul class='horizontal-list'>" + namesListHtml + "</ul>";
 }
 
-document.getElementById("namesCuteFriendsBtn").onclick = displayNameCuteFriends ; 
+document.getElementById("namesCuteFriendsBtn myBtn").onclick = displayNameCuteFriends ; 
 
 // array.filter() = 
 // pricings = 10000, 20000, 30000, 40000, 50000, 60000 
@@ -57,7 +57,7 @@ function displayPrice () {
 }
 
 
-document.getElementById("cehckPriceBtn").onclick = displayPrice ; 
+document.getElementById("cehckPriceBtn myBtn").onclick = displayPrice ; 
 
 // array.reduce() = to sum up 
 let checkOutPrices = [6000, 8000, 9900, 13000];
@@ -73,7 +73,7 @@ function displayCheckOutprice() {
     document.getElementById("checkOutPriceLabel").innerHTML = "le prix total: JPY" + total + "." ; 
 }
 
-document.getElementById("checkOutPriceBtn").onclick = displayCheckOutprice ;
+document.getElementById("checkOutPriceBtn myBtn").onclick = displayCheckOutprice ;
 
 // sort an array of numbers array.sort()
 
@@ -94,7 +94,7 @@ function displayCheckOutPricesDes() {
     document.getElementById("checkOutPriceDesLabel").innerHTML = "la commande la plus chère -> la commande la moins chère: JPY" + checkOutPriceDes.join("> JPY") + "."; 
 }
 
-document.getElementById("checkOutPriceDesBtn").onclick = displayCheckOutPricesDes;
+document.getElementById("checkOutPriceDesBtn myBtn").onclick = displayCheckOutPricesDes;
 
 // traditional function expression = unique expression one time use 
 const checkProductDescription = function() {
@@ -107,11 +107,64 @@ function displayProductDescription() {
     document.getElementById("checkPrdocutDescriptionLabel").textContent = "description du produit: " + displayProductDescriptionText + "." ; 
 }
 
-document.getElementById("checkPrdocutDescriptionBtn").onclick = displayProductDescription;
-
-// arrow function 
-
-// const greeting = (username) => console.log(`bonjour ${username}`);
-//greeting('yuru');
+document.getElementById("checkPrdocutDescriptionBtn myBtn").onclick = displayProductDescription;
 
 
+// check this one later 
+// multiple buttons on a page with Enter key  
+const buttons = document.querySelectorAll("votre-nom-btn myBtn");
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    buttons.forEach(button => button.click());
+  }
+  document.addEventListener("keypress", handleKeyPress);
+}
+
+// shuffle array 
+
+function shuffleProducts() {
+let randomProductRecommand = ["earrings", "bangles", "pendantNecks", "rings", "necklace", "studs"]; 
+shuffle(randomProductRecommand);
+return randomProductRecommand.join(", ");
+
+function shuffle(array) {
+    let currentIndex = array.length; 
+
+    while(currentIndex !=0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -=1;
+        
+        let temp = array[currentIndex]; 
+        array[currentIndex] = array[randomIndex]; 
+        array[randomIndex] = temp; 
+    }
+
+    return array ; 
+} 
+}
+
+document.getElementById("comingUpProductsBtn myBtn").onclick = function() {
+    document.getElementById("comingUpProductsLabel").innerHTML = "que vais-je faire en premier: " + shuffleProducts();
+}
+
+// ted functions ex: login page 
+let loginName = (document.getElementById("loginName")).value;
+
+function login(){
+loginPageMessage1();
+loginPageMessage2();
+
+function loginPageMessage1() {
+    document.getElementById("loginPageMessage1").innerHTML = `bienvenue ${loginName}.` ; 
+}
+
+function loginPageMessage2() {
+    document.getElementById("loginPageMessage2").innerHTML = `comment vous sentez-vous aujourd’hui ?`;
+}
+}
+
+document.getElementById("loginPageBtn").onclick = function() {
+    login();
+}
+
+// 48.maps
